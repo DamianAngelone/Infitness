bf2034f8e348c001ada8fc3782e32ac468a2895f
Viraj_Janakkumar_Patel <patelvj2@mcmaster.ca>
2017-04-06 19:21:52 -0400

    Finished Side menu food tracker

--- Infitness/app/src/main/java/comdamianangeloneinfitness/httpsgithub/infitness/MainActivity.java
+++ Infitness/app/src/main/java/comdamianangeloneinfitness/httpsgithub/infitness/MainActivity.java
@@ -3,6 +3,8 @@ package comdamianangeloneinfitness.httpsgithub.infitness;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,6 +24,15 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = new Intent(this, Login.class);
         startActivity(intent);
+
+        Button nutrition = (Button)findViewById(R.id.NutritionTracker);
+        nutrition.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, FoodTracker.class));
+            }
+        });
+
     // Example of a call to a native method
    // TextView tv = (TextView) findViewById(R.id.sample_text);
     //tv.setText(stringFromJNI());
