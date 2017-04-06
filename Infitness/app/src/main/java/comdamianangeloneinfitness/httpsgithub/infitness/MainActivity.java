5dc643c6539afbd1caa45b101878fd5cb500e48c
DamianAngelone <d-angelone@hotmail.com>
2017-04-06 19:33:00 -0400

    pedometer added

--- Infitness/app/src/main/java/comdamianangeloneinfitness/httpsgithub/infitness/MainActivity.java
+++ Infitness/app/src/main/java/comdamianangeloneinfitness/httpsgithub/infitness/MainActivity.java
@@ -1,5 +1,6 @@
 package comdamianangeloneinfitness.httpsgithub.infitness;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,13 +8,28 @@ import android.widget.TextView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
     FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     //DatabaseReference ref = firebaseDatabase.getReference("Fk me");
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    private boolean isSensorPresent = false;
+    private TextView mStepsSinceReboot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +39,66 @@ public class MainActivity extends AppCompatActivity{
 
         Intent intent = new Intent(this, Login.class);
         startActivity(intent);
+
+        mSensorManager = (SensorManager)  this.getSystemService(Context.SENSOR_SERVICE);
+        Button clickButton = (Button) findViewById(R.id.walkController);
+        clickButton.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                setContentView(R.layout.pedometer_view);
+                mStepsSinceReboot = (TextView) findViewById(R.id.steps);
+                if(mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+                        != null)
+                {
+                    mSensor =
+                            mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+                    isSensorPresent = true;
+                }
+                else
+                {
+                    isSensorPresent = false;
+                }
+
+
+            }
+        });
     // Example of a call to a native method
    // TextView tv = (TextView) findViewById(R.id.sample_text);
     //tv.setText(stringFromJNI());
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(isSensorPresent)
+        {
+            mSensorManager.registerListener(this, mSensor,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(isSensorPresent)
+        {
+            mSensorManager.unregisterListener(this);
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        mStepsSinceReboot.setText(String.valueOf(event.values[0]));
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int integer) {
+
+
+    }
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
